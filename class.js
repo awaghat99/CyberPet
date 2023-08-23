bb9433107e4204c288611ecdc12708716fa69197
Zaid <zedpx101@outlook.com>
2023-08-23 16:25:03 +0100

    initial javascript

---
+++ class.js
@@ -0,0 +1,37 @@
+class Animals {
+    constructor () {
+        this.health = 100;
+        this.hunger = 100;
+        this.thirst = 100;
+    }
+    depleting(property) {
+        setInterval(() => {
+            property -= 5; 
+        }, 500)
+    } 
+    eat () {
+        this.hunger += 10;
+    }
+    drink () {
+        this.thirst += 10;
+    }
+}
+
+class Dog extends Animals {
+    constructor () {
+        super ();
+        this.happiness = 100;
+    }
+    fetch () {
+        this.happiness += 10;
+        this.hunger -= 10;
+        this.thirst -= 10;
+    }
+    walk() {
+        this.happiness += 10;
+        this.hunger -= 20;
+        this.thirst -= 20;
+    }    
+}
+
+module.exports = {Animals, Dog}
---
+++ script.js
@@ -0,0 +1,23 @@
+// const animalClasses = require("./class.js")
+const readyBtn = document.getElementById('readyButton')
+const radioButtons = document.querySelectorAll('input[name="pet"');
+const petPage = document.getElementById('pet-pages')
+const mainMenu = document.getElementById('mainMenu')
+
+readyBtn.addEventListener('click', () => {
+    mainMenu.style.display = "none";
+    petPage.style.display = "flex";
+})
+
+// readyBtn.addEventListener('click',() => {
+//     let selectedPet;
+//     for(let radioButton of radioButtons) {
+//         if (radioButton.checked) {
+//             selectedPet = radioButton.value;
+//             break;
+//         }
+//     }
+//     if (selectedPet == "dog") {
+//         const dog = new animalClasses.Dog
+//     }
+// })
