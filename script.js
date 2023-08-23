75d960f8d1338e30bf715c184a73c79975bf5ddb
George Tinsley <george8s@live.co.uk>
2023-08-23 17:29:19 +0100

    added subclasses

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // const animalClasses = require("./class.js")
-const readyBtn = document.getElementById('readyButton')
+const readyBtn = document.getElementById('readyButton');
 const radioButtons = document.querySelectorAll('input[name="pet"');
-const petPage = document.getElementById('pet-pages')
-const mainMenu = document.getElementById('mainMenu')
+const petPage = document.getElementById('pet-pages');
+const mainMenu = document.getElementById('mainMenu');
+const inputName = document.getElementById("inputName");
+const healthBar = document.getElementsByClassName("health-bar");
 
 readyBtn.addEventListener('click', () => {
     mainMenu.style.display = "none";
     petPage.style.display = "flex";
-})
+});
 
 // readyBtn.addEventListener('click',() => {
 //     let selectedPet;
@@ -21,3 +23,21 @@ readyBtn.addEventListener('click', () => {
 //         const dog = new animalClasses.Dog
 //     }
 // })
+
+// // code for when bar levels changing set interval?
+// for (healthBar >= 70){
+// }
+
+
+// // code for when ok
+// for (healthBar < 70 && healthBar > 40){
+// }
+
+// // code for when sad
+// for (healthBar <= 40){
+// }
+
+// // code for when you lose(timer for total time?), lose popup, play again
+// if (healthBar = 0){
+
+// };
