5a46e10073b08a6aced238d98faa39b358b67ee4
Zaid <zedpx101@outlook.com>
2023-08-23 17:16:38 +0100

    back to menu button

--- script.js
+++ script.js
@@ -3,21 +3,27 @@ const readyBtn = document.getElementById('readyButton')
 const radioButtons = document.querySelectorAll('input[name="pet"');
 const petPage = document.getElementById('pet-pages')
 const mainMenu = document.getElementById('mainMenu')
+const backButton = document.getElementById('backButton')
 
-readyBtn.addEventListener('click', () => {
-    mainMenu.style.display = "none";
-    petPage.style.display = "flex";
+// readyBtn.addEventListener('click', () => {
+//     mainMenu.style.display = "none";
+//     petPage.style.display = "flex";
+// })
+
+readyBtn.addEventListener('click',() => {
+    let selectedPet;
+    for(let radioButton of radioButtons) {
+        if (radioButton.checked) {
+            selectedPet = radioButton.value;
+            break;
+        }
+    }
+    if (selectedPet == "dog") {
+        const dog = new animalClasses.Dog
+    }
 })
 
-// readyBtn.addEventListener('click',() => {
-//     let selectedPet;
-//     for(let radioButton of radioButtons) {
-//         if (radioButton.checked) {
-//             selectedPet = radioButton.value;
-//             break;
-//         }
-//     }
-//     if (selectedPet == "dog") {
-//         const dog = new animalClasses.Dog
-//     }
-// })
+backButton.addEventListener('click', () => {
+    mainMenu.style.display = "block";
+    petPage.style.display = "none";
+})
