a7b51b0b4a6dacdca80c2d94afc0b61541d835ec
awaghat99 <awaghat99@gmail.com>
2023-08-24 10:05:46 +0100

    minor changes

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ const petPage = document.getElementById('pet-pages')
 const mainMenu = document.getElementById('mainMenu')
 const backButton = document.getElementById('backButton')
 
-// readyBtn.addEventListener('click', () => {
-//     mainMenu.style.display = "none";
-//     petPage.style.display = "flex";
-// })
-
-readyBtn.addEventListener('click',() => {
-    let selectedPet;
-    for(let radioButton of radioButtons) {
-        if (radioButton.checked) {
-            selectedPet = radioButton.value;
-            break;
-        }
-    }
-    if (selectedPet == "dog") {
-        const dog = new animalClasses.Dog
-    }
+readyBtn.addEventListener('click', () => {
+    mainMenu.style.display = "none";
+    petPage.style.display = "flex";
 })
+
+// readyBtn.addEventListener('click',() => {
+//     let selectedPet;
+//     for(let radioButton of radioButtons) {
+//         if (radioButton.checked) {
+//             selectedPet = radioButton.value;
+//             break;
+//         }
+//     }
+//     if (selectedPet == "dog") {
+//         const dog = new animalClasses.Dog
+//     }
+// })
 
 backButton.addEventListener('click', () => {
     mainMenu.style.display = "block";
