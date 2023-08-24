f6c3d59822810cfd569b045cff850c6a31822f51
Zaid <zedpx101@outlook.com>
2023-08-24 11:13:56 +0100

    eat button to function with hunger bar

--- class.js
+++ class.js
@@ -34,4 +34,4 @@ class Dog extends Animals {
     }    
 }
 
-module.exports = {Animals, Dog}
+// module.exports = Animals;
--- script.js
+++ script.js
@@ -1,29 +1,62 @@
-// const animalClasses = require("./class.js")
+// const animalClasses = require("./class.js");
 const readyBtn = document.getElementById('readyButton')
 const radioButtons = document.querySelectorAll('input[name="pet"');
 const petPage = document.getElementById('pet-pages')
 const mainMenu = document.getElementById('mainMenu')
 const backButton = document.getElementById('backButton')
+const healthBar = document.getElementById('health')
+const hungerBar = document.getElementById('hunger')
+const thirstBar = document.getElementById('thirst')
+const eatButton = document.getElementById('eatButton')
+const drinkButton = document.getElementById('drinkButton')
 
-// readyBtn.addEventListener('click', () => {
-//     mainMenu.style.display = "none";
-//     petPage.style.display = "flex";
+readyBtn.addEventListener('click', () => {
+    mainMenu.style.display = "none";
+    petPage.style.display = "flex";
+})
+
+let currentHunger = 100;
+
+const updateHungerBar = () => {
+    const hungerPercentage = (currentHunger / 100) * 100;
+    hungerBar.style.width = hungerPercentage + "%";
+}
+
+const decreaseHunger = () => {
+    if (currentHunger > 0) {
+        currentHunger -= 1;
+        updateHungerBar();
+        setTimeout(decreaseHunger, 1000)
+    }
+}
+
+const increaseHunger = () => {
+    if (currentHunger < 100) {
+        currentHunger += 10;
+        updateHungerBar();
+    }
+}
+
+eatButton.addEventListener('click, increaseHunger');
+decreaseHunger();
+
+
+
+
+// readyBtn.addEventListener('click',() => {
+//     let selectedPet;
+//     for(let radioButton of radioButtons) {
+//         if (radioButton.checked) {
+//             selectedPet = radioButton.value;
+//             break;
+//         }
+//     }
+//     if (selectedPet == "dog") {
+//         const dog = new animalClasses.Dog
+//     }
 // })
 
-readyBtn.addEventListener('click',() => {
-    let selectedPet;
-    for(let radioButton of radioButtons) {
-        if (radioButton.checked) {
-            selectedPet = radioButton.value;
-            break;
-        }
-    }
-    if (selectedPet == "dog") {
-        const dog = new animalClasses.Dog
-    }
-})
-
-backButton.addEventListener('click', () => {
-    mainMenu.style.display = "block";
-    petPage.style.display = "none";
-})
+// backButton.addEventListener('click', () => {
+//     mainMenu.style.display = "block";
+//     petPage.style.display = "none";
+// })
